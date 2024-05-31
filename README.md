2393f9a0ee3db29e89c589b75922365ec3351bcb
aadil10i <aadil3003@outlook.com>
2024-05-31 21:17:18 +0400

    faq fixes

--- README.md
+++ README.md
@@ -2,15 +2,15 @@ Todo
 
 - [x] fix landing text and background
 - [x] add book a call button in landing
-- [ ] contact us buttons go to contact form
+- [ ] Create contact Page
 - [x] add number in footer and company info (later)
 - [ ] Showcase
 - [x] Fix companis
-- [ ] Make FAQ Own route
-- [ ] fix pricing
+- [x] Make FAQ Own route
+- [x] fix pricing
   - [x] section in home page
   - [x] text content
-  - [ ] auth
+  - [x] auth
 - [x] remove blog
 - [x] remove auth
 - [x] remove language
--- apps/nextjs/src/components/price/pricing-faq.tsx
+++ apps/nextjs/src/components/price/pricing-faq.tsx
@@ -7,36 +7,68 @@ import {
   AccordionTrigger,
 } from "@saasfly/ui/accordion";
 
-import type { Locale } from "~/config/i18n-config";
-import { priceFaqDataMap } from "~/config/price/price-faq-data";
-
-export function PricingFaq({
-  params: { lang },
-  dict,
-}: {
-  params: {
-    lang: Locale;
-  };
-  dict: Record<string, string>;
-}) {
-  const pricingFaqData = priceFaqDataMap[lang];
+export function PricingFaq() {
   return (
     <section className="container max-w-3xl py-2">
       <div className="mb-14 space-y-6 text-center">
         <h1 className="text-center font-heading text-3xl md:text-5xl">
-          <Balancer>{dict.faq}</Balancer>
+          <Balancer>Frequently Asked Questions</Balancer>
         </h1>
         <p className="text-md text-muted-foreground">
-          <Balancer>{dict.faq_detail}</Balancer>
+          <Balancer>
+            Explore our comprehensive FAQ to find quick answers to common
+            inquiries. If you need further assistance, don&apos;t hesitate to
+            contact us for personalized help.
+          </Balancer>
         </p>
       </div>
       <Accordion type="single" collapsible className="w-full">
-        {pricingFaqData?.map((faqItem) => (
-          <AccordionItem key={faqItem.id} value={faqItem.id}>
-            <AccordionTrigger>{faqItem.question}</AccordionTrigger>
-            <AccordionContent>{faqItem.answer}</AccordionContent>
-          </AccordionItem>
-        ))}
+        {/* {pricingFaqData?.map((faqItem) => ( */}
+        <AccordionItem value="item1">
+          <AccordionTrigger>
+            What is the cost of the free plan?
+          </AccordionTrigger>
+          <AccordionContent>
+            Our free plan is completely free, with no monthly or annual charges.
+            Its a great way to get started and explore our basic features.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item2">
+          <AccordionTrigger>
+            How much does the Pro Monthly plan cost?
+          </AccordionTrigger>
+          <AccordionContent>
+            The Pro Monthly plan is priced at $30 per month. It provides access
+            to our core features and is billed on a monthly basis.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item3">
+          <AccordionTrigger>
+            What is the cost of the free plan?
+          </AccordionTrigger>
+          <AccordionContent>
+            Our free plan is completely free, with no monthly or annual charges.
+            Its a great way to get started and explore our basic features.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item4">
+          <AccordionTrigger>
+            What is the cost of the free plan?
+          </AccordionTrigger>
+          <AccordionContent>
+            Our free plan is completely free, with no monthly or annual charges.
+            Its a great way to get started and explore our basic features.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item5">
+          <AccordionTrigger>
+            What is the cost of the free plan?
+          </AccordionTrigger>
+          <AccordionContent>
+            Our free plan is completely free, with no monthly or annual charges.
+            Its a great way to get started and explore our basic features.
+          </AccordionContent>
+        </AccordionItem>
       </Accordion>
     </section>
   );
